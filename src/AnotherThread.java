89938344c4241288eae54108f8aee2276158c8d0
sharan.ka <karthikeyansharan00@gmail.com>
2022-10-30 20:20:16 +0530

    set a name for a thread to see, in which thread is running

--- src/AnotherThread.java
+++ src/AnotherThread.java
@@ -1,6 +1,6 @@
 public class AnotherThread extends Thread {
     @Override
     public void run() {
-        System.out.println(ThreadColor.ANSI_BLUE + "Hello from the another thread.");
+        System.out.println(ThreadColor.ANSI_BLUE + "Hello from " + currentThread().getName());
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,10 @@ public class Main {
         System.out.println(ThreadColor.ANSI_PURPLE + "Hello from the main thread!!");
 
         Thread anotherThread = new AnotherThread();
+        anotherThread.setName("== Another Thread ==");
         anotherThread.start();
+        // Always start a thread using `start` method. Don't directly call the `run` method.
+        // If you call `run` directly, then the thread will run on `main` thread. Just give a trail by changing the anotherThread.start(); to anotherThread.run(); and play.
 
         new Thread() {
             public void run() {
@@ -12,10 +15,10 @@ public class Main {
         }.start();
 
 //        Thread myRunnableThread = new Thread(new MyRunnable());
-        Thread myRunnableThread = new Thread(new MyRunnable(){
+        Thread myRunnableThread = new Thread(new MyRunnable() {
             @Override
             public void run() {
-                System.out.println(ThreadColor.ANSI_RED+"Hello from the anonymous class's implementation of run()");
+                System.out.println(ThreadColor.ANSI_RED + "Hello from the anonymous class's implementation of run()");
             }
         });
         myRunnableThread.start();
