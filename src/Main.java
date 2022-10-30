7220167216c6fadcecabc49bd59e2fdf5252d8b6
sharan.ka <karthikeyansharan00@gmail.com>
2022-10-30 19:15:59 +0530

    passing anonymous myrunnable instance to thread constructor

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,13 @@ public class Main {
             }
         }.start();
 
-        Thread myRunnableThread = new Thread(new MyRunnable());
+//        Thread myRunnableThread = new Thread(new MyRunnable());
+        Thread myRunnableThread = new Thread(new MyRunnable(){
+            @Override
+            public void run() {
+                System.out.println(ThreadColor.ANSI_RED+"Hello from the anonymous class's implementation of run()");
+            }
+        });
         myRunnableThread.start();
 
         System.out.println(ThreadColor.ANSI_PURPLE + "Hello again from the main thread!!");
