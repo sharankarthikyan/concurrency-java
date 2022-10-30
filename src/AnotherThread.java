df9e819af46440ba8af03e6934b8473b7fade4e5
sharan.ka <karthikeyansharan00@gmail.com>
2022-10-30 20:20:40 +0530

    added sleep in another thread

--- src/AnotherThread.java
+++ src/AnotherThread.java
@@ -2,5 +2,13 @@ public class AnotherThread extends Thread {
     @Override
     public void run() {
         System.out.println(ThreadColor.ANSI_BLUE + "Hello from " + currentThread().getName());
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            System.out.println(ThreadColor.ANSI_BLUE + "Another thread woke up");
+        }
+
+        System.out.println(ThreadColor.ANSI_BLUE + "Three seconds have passed and I'm awake");
     }
 }
