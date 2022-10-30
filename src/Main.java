94cfdf6e4a66c3011ce4771e0a822e6fc907b261
sharan.ka <karthikeyansharan00@gmail.com>
2022-10-30 21:18:56 +0530

    setting up this branch for multiple threading operations

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(ThreadColor.ANSI_PURPLE + "Hello from the main thread!!");
 
-        Thread anotherThread = new AnotherThread();
-        anotherThread.setName("== Another Thread ==");
-        anotherThread.start();
-        // Always start a thread using `start` method. Don't directly call the `run` method.
-        // If you call `run` directly, then the thread will run on `main` thread. Just give a trail by changing the anotherThread.start(); to anotherThread.run(); and play.
-
-        new Thread() {
-            public void run() {
-                System.out.println(ThreadColor.ANSI_GREEN + "Hello from the anonymous thread.");
-            }
-        }.start();
-
-//        Thread myRunnableThread = new Thread(new MyRunnable());
-        Thread myRunnableThread = new Thread(new MyRunnable() {
-            @Override
-            public void run() {
-                System.out.println(ThreadColor.ANSI_RED + "Hello from the anonymous class's implementation of run()");
-                try {
-                    anotherThread.join(2000); // If any thread calls join method of some other thread,
-                    // then current will wait for other thread execution.
-                    // Once it gets complete then current thread will start executing again.
-                    System.out.println(ThreadColor.ANSI_RED + "Another thread terminated, or timed out, so I'm running again.");
-                } catch (InterruptedException e) {
-                    System.out.println(ThreadColor.ANSI_RED + "I couldn't wait after all. I was interrupted.");
-                }
-            }
-        });
-        myRunnableThread.start();
-
-//        anotherThread.interrupt();
-
-        System.out.println(ThreadColor.ANSI_PURPLE + "Hello again from the main thread!!");
     }
 }
--- src/MyRunnable.java
+++
@@ -1,6 +0,0 @@
-public class MyRunnable implements Runnable {
-    @Override
-    public void run() {
-        System.out.println(ThreadColor.ANSI_RED + "Hello from my runnable");
-    }
-}
