e449436bef117a4627ae9bf83abd3c4905b5266a
sharan.ka <karthikeyansharan00@gmail.com>
2022-11-01 19:52:17 +0530

    2 threads processing a 2 different object

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        Countdown countdown = new Countdown();
+        Countdown countdown1 = new Countdown();
+        Countdown countdown2 = new Countdown();
 
-        CountdownThread t1 = new CountdownThread(countdown);
+        CountdownThread t1 = new CountdownThread(countdown1);
         t1.setName("Thread 1");
-        CountdownThread t2 = new CountdownThread(countdown);
+        CountdownThread t2 = new CountdownThread(countdown2);
         t2.setName("Thread 2");
 
         t1.start();
