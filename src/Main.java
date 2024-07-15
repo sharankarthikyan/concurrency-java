ef78e44b3007104060206876c231daa634fe7dae
Sharan Karthikeyan <43072292+sharankarthikyan@users.noreply.github.com>
2024-07-15 17:42:39 +0530

    Deadlock

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,37 @@
 public class Main {
+    private static final Object lock1 = new Object();
+    private static final Object lock2 = new Object();
+
     public static void main(String[] args) {
-        System.out.println(ThreadColor.ANSI_PURPLE + "Hello from the main thread!!");
+        Thread thread1 = new Thread(() -> {
+            synchronized (lock1) {
+                System.out.println("Thread 1: Holding lock 1...");
 
-        Thread anotherThread = new AnotherThread();
-        anotherThread.setName("== Another Thread ==");
-        anotherThread.start();
-        // Always start a thread using `start` method. Don't directly call the `run` method.
-        // If you call `run` directly, then the thread will run on `main` thread. Just give a trail by changing the anotherThread.start(); to anotherThread.run(); and play.
+                // Simulate some work with lock 1
+                try { Thread.sleep(100); } catch (InterruptedException e) {}
 
-        new Thread() {
-            public void run() {
-                System.out.println(ThreadColor.ANSI_GREEN + "Hello from the anonymous thread.");
-            }
-        }.start();
-
-//        Thread myRunnableThread = new Thread(new MyRunnable());
-        Thread myRunnableThread = new Thread(new MyRunnable() {
-            @Override
-            public void run() {
-                System.out.println(ThreadColor.ANSI_RED + "Hello from the anonymous class's implementation of run()");
-                try {
-                    anotherThread.join(2000); // If any thread calls join method of some other thread,
-                    // then current will wait for other thread execution.
-                    // Once it gets complete then current thread will start executing again.
-                    System.out.println(ThreadColor.ANSI_RED + "Another thread terminated, or timed out, so I'm running again.");
-                } catch (InterruptedException e) {
-                    System.out.println(ThreadColor.ANSI_RED + "I couldn't wait after all. I was interrupted.");
+                System.out.println("Thread 1: Waiting for lock 2...");
+                synchronized (lock2) {
+                    System.out.println("Thread 1: Acquired lock 2!");
                 }
             }
         });
-        myRunnableThread.start();
 
-//        anotherThread.interrupt();
+        Thread thread2 = new Thread(() -> {
+            synchronized (lock2) {
+                System.out.println("Thread 2: Holding lock 2...");
 
-        System.out.println(ThreadColor.ANSI_PURPLE + "Hello again from the main thread!!");
+                // Simulate some work with lock 2
+                try { Thread.sleep(100); } catch (InterruptedException e) {}
+
+                System.out.println("Thread 2: Waiting for lock 1...");
+                synchronized (lock1) {
+                    System.out.println("Thread 2: Acquired lock 1!");
+                }
+            }
+        });
+
+        thread1.start();
+        thread2.start();
     }
 }
